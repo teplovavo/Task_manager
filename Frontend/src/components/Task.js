9409d3dd0edd327e7dc63fb7b88034706e8cb072
Veronika Teplova <teplovavo@gmail.com>
2024-11-20 10:11:52 -0700

    debugged

--- Frontend/src/components/Task.js
+++ Frontend/src/components/Task.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// Component to display a task
-function Task({ task, deleteTask }) {
+function Task({ task, deleteTask, editTask }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedDescription, setEditedDescription] = useState(task.description);
+
+  // edit function
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  // function to save edited task
+  const handleSave = () => {
+    editTask(task._id, editedDescription);
+    setIsEditing(false);
+  };
+
+  // function to cancel editing
+  const handleCancel = () => {
+    setIsEditing(false);
+    setEditedDescription(task.description);
+  };
+
   return (
     <li>
-      {task.description}
-      {/* Button to delete the task */}
-      <button onClick={deleteTask}>Delete</button>
+      {isEditing ? (
+        <>
+          <input
+            type="text"
+            value={editedDescription}
+            onChange={(e) => setEditedDescription(e.target.value)}
+          />
+          <button onClick={handleSave}>Save</button>
+          <button onClick={handleCancel}>Cancel</button>
+        </>
+      ) : (
+        <>
+          {task.description}
+          <button onClick={handleEdit}>Edit</button>
+          <button onClick={deleteTask}>Delete</button>
+        </>
+      )}
     </li>
   );
 }
