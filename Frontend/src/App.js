5b527f0d74cd9667ba530d88019813a82118b93e
Veronika Teplova <teplovavo@gmail.com>
2024-11-22 13:51:42 -0700

    connecting code

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -65,4 +65,13 @@ function App() {
   );
 }
 
+const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
+
+axios.get(`${backendUrl}/api/tasks`)
+  .then(response => { /* ... */ })
+  .catch(error => { /* ... */ });
+
+
+
+
 export default App;
